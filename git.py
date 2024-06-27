43783b8d616b8325cbd1eb2bc0bc8a50fd3026a1
Anjali Tiwari <ATiwari14@slb.com>
2024-06-27 22:58:22 +0530

    Automated file transfer from Azure Blob Storage

--- git.py
+++ git.py
@@ -3,6 +3,7 @@ import subprocess
 subprocess.run(["git", "init"])
 subprocess.run(["git", "remote", "add", "origin", "https://github.com/hashdash13/azure.git"])
 subprocess.run(["git", "add", "."])
+subprocess.run(["git", "config", "--global", "credential.helper", "store"])
 subprocess.run(["git", "checkout", "-b", "branch_name"])
 subprocess.run(["git", "commit", "-m", "Automated file transfer from Azure Blob Storage"])
 subprocess.run(["git", "push", "-u", "origin", "branch_name"])
